d53d4ca668025a0c673df049ba3ed747e40cba02
MelaUswatunHasanah <melauswatun28@gmail.com>
2021-09-27 23:39:38 +0700

    menambahkan tugas2 dan biodata diri

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya : Mela Uswatun Hasanah
+        Alamat : Bantargedang RT/RW 04/09 Kel.Kersanagara Kec.Cibeureum Kota Tasikmalaya
+        Hobi : Masak
+        No.hp : 085314636377"
+    }
+}
